05918b1d393b979cb87ef501c149eb669d653383
yuta-komura <esfgshdfsd@gmail.com>
2024-12-29 13:37:52 +0900

    依存関係の整理とStylelint設定の移行

--- stylelint.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ['stylelint-config-standard'], // 標準ルールセット
-  plugins: ['stylelint-order'], // プロパティ順序を管理
-  rules: {
-    'order/properties-alphabetical-order': true, // プロパティをアルファベット順に
-  },
-};
